7feccd29d12deb8539e5331052767015aedaedd8
123 <123@qq.com>
2018-11-24 20:49:25 +0800

    第二次

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
         System.out.println("Hello world");
         System.out.println("123");
         System.out.println("小星星哦");
