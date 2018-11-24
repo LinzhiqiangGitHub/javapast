cadf21418e044a5896648eeca02d32fbf7172830
123 <123@qq.com>
2018-11-24 17:45:38 +0800

    test11

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("Hello world");
         System.out.println("123");
+        System.out.println("小星星哦");
+        System.out.println("hahahah");
     }
 }
